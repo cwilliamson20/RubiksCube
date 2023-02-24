b23c6f7b9b2a4c684a983a8c2943db4ae175309c
Connie Williamson <connie.jane.williamson@gmail.com>
2023-02-23 19:05:31 -0800

    minimum working glfw with blank window

--- main.cxx
+++ main.cxx
@@ -1,3 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+using std::cout;
+#define GLFW_INCLUDE_NONE
+#include <GL/glew.h>    // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h> // GLFW helper library
+
+GLFWwindow* setUpAndCreateWindow() {
+    if (!glfwInit()) {
+        // initialization of GLFW failed
+        cout << "Could not start GLFW\n";
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // create the window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Rubik's Cube Simulator", NULL, NULL);
+    // make sure there weren't any error creating the window
+    if (!window) {
+        cout << "Could not create the window\n";
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    return window;
+}
+
+
 int main() {
-    
+    GLFWwindow *window = setUpAndCreateWindow();
+    while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+        // keep running the program because the user hasn't tried to close the main window yet
+        glfwPollEvents();
+
+    }
+
+    glfwDestroyWindow(window);
+
+
+
+    glfwTerminate();
 }
