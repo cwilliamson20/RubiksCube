f15b85dca940cbcf6882c609ea88944948cf18cd
Connie Williamson <connie.jane.williamson@gmail.com>
2023-02-23 18:34:09 -0800

    set up directory and made main file

---
+++ CMakeFiles/rubiks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rubiks.dir/main.cxx.o"
+  "rubiks"
+  "rubiks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rubiks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
