fdc27ffa2e3a9685db16072871a08e6edad41a44
Connie Williamson <connie.jane.williamson@gmail.com>
2023-03-02 15:06:09 -0800

    finished hello triangle for learn opengl

--- vertexshader.glsl
+++ vertexshader.glsl
@@ -1,18 +1,13 @@
 #version 400
 // Input vertex data, different for all executions of this shader.
-layout(location = 0) in vec3 vertexPosition_modelspace;
-
-// input color data, the second buffer (location = 1)
-layout(location = 1) in vec3 vertexColor;
+layout(location = 0) in vec3 aPos;
 
 // output data that the fragment shader uses
 out vec3 fragment_color;
-// Values that stay constant for the whole mesh.
-uniform mat4 MVP;
 
 void main(){
 
 	// Output position of the vertex, in clip space : MVP * position
-	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
-    fragment_color = vertexColor;
+	gl_Position =  vec4(aPos,1);
+    fragment_color = vec3(0, 1, 1);
 }
